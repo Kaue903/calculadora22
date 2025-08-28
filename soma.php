9878921cc1ca81e58961a84dcb8f0c3ff7b5b423
Kaue903 <kauer903@gmail.com>
2025-08-28 14:52:59 -0300

    commit do kaue soma

--- soma.php
+++ soma.php
@@ -1,20 +1,44 @@
-<!DOCTYPE html>
 <html>
-<head>
-    <meta charset='utf-8'>
-    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
-    <title>Page Title</title>
-    <meta name='viewport' content='width=device-width, initial-scale=1'>
-    <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
-    <script src='main.js'></script>
-</head>
-<body>
+    <head>
+        <meta charset="UTF-8">
+        <title>Calculadora Simples</title>
+    </head>
+    <body>
+        <bgcolor="#6edfe3ff">
+        <h1>Calculadora Simples</h1>
+        <form method="GET" action="">
+            <label for="num1">Número1:</label><br>
+            <input type="text" name="n1"><br>
+            <label for="num2">Número2:</label><br>
+            <input type="text" name="n2"><br>
+            <input type="submit" value="Calcular">
+            <fieldset style="margin-right: 1000px;">
+                <legend>Operações</legend>
+                <input type="radio" name="op" value="soma" checked>Soma<br>
+            </fieldset>
+        </form>
+        
+        <?php
+            if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+                if (isset($_GET['n1']) && isset($_GET['n2']) && is_numeric($_GET['n1']) && is_numeric($_GET['n2'])) {
+                    $n1 = $_GET['n1'];
+                    $n2 = $_GET['n2'];
 
-    <?php
-        $total = $x = $y;
-        echo "<h1> Total: " . $x . "</h1>";
-    ?>
+                    // Funções para as operações
+                    function soma($n1, $n2) {
+                        return $n1 + $n2;
+                    
+                    }
 
-    
-</body>
+                    // Exibir o resultado conforme a operação escolhida
+                    if ($_GET['op'] == 'soma') {
+                        echo "<h2>Resultado: $n1 + $n2 = " . soma($n1, $n2) . "</h2>";
+                    } elseif ($_GET['op'] == 'subtração') {
+                    }
+                } else {
+                    echo "<h2>Por favor, insira números válidos.</h2>";
+                }
+            }
+        ?>
+    </body>
 </html>
